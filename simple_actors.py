8cd7306f149086efdc28d2678ae952d07d20ec55
Jakub Urban <urban@ipp.cas.cz>
2013-07-18 16:51:50 +0200

    using dicts for ports and in/output

--- simple_actors.py
+++ simple_actors.py
@@ -1,51 +1,37 @@
 class ActorA(object):
     """docstring for ActorA"""
-    def __init__(self, config):
+    def __init__(self, config, in_ports=('input_1', ), out_ports=('output_1', )):
         super(ActorA, self).__init__()
         self.config = config
-        self._input_1 = None
+        self.in_ports = list(in_ports)
+        self.out_ports = list(out_ports)
         # list of connected ports
         # results_id -> [target1, target2, ...]
         self.connections = {}
         self.results = {}
+        self.inputs = {}
+        self.clear_inputs()
 
-    @property
-    def input_1(self):
-        print('input1 getter')
-        if len(self._input_1) > 1:
-            return self._input_1
-        elif len(self._input_1) > 0:
-            return self._input_1[0]
-        return None
-
-    @input_1.setter
-    def input_1(self, value):
-        print('input1 setter')
-        if self._input_1 is not None:
-            self._input_1.append(value)
-        else:
-            self._input_1 = [value]
+    def put_input(self, port, value):
+        self.inputs[port].append(value)
         self.eval_inputs()
-
-    @input_1.deleter
-    def input_1(self):
-        print('input1 deleter')
-        self._input_1 = []
 
     def eval_inputs(self):
         # input complete logic
-        if self._input_1 is not None:
+        if self.inputs.get('input_1'):
             self.fire()
 
     def clear_inputs(self):
         print('clear inputs')
-        del self.input_1
+        for port in self.in_ports:
+            self.inputs[port] = []
 
     def fire(self, input_1=None):
         print('inside fire')
-        if input_1 is None:
-            input_1 = self._input_1
-        self.results['result_1'] = [self.config * i for i in input_1]
+        self.results['result_1'] = 0
+        for i, v in self.inputs.iteritems():
+            for vv in v:
+                self.results['result_1'] += self.config * vv
         self.clear_inputs()
         print('result = %s' % self.results)
         # for connection, value_key in self._output_1:
@@ -54,5 +40,8 @@ class ActorA(object):
 
 if __name__ == '__main__':
     a = ActorA(3)
-    a.input_1 = 3
+    a.put_input('input_1', 3)  # keep a.input_1 = 3 ???
+
+    b = ActorA(-2)
+    a.connections['result_1'] = [{'actor': b, 'port': 'port_1'}]
 

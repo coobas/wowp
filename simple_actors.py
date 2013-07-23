caa5da5641ad136aa0cf813a7bc4a9a8620a5999
Jakub Urban <urban@ipp.cas.cz>
2013-07-23 22:26:36 +0200

    created Actor super class

--- simple_actors.py
+++ simple_actors.py
@@ -3,44 +3,56 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-class ActorA(object):
-    """docstring for ActorA"""
+class Actor(object):
+    """Actor base class"""
 
     count = 0
 
-    def __init__(self, name, config, in_ports=('input_1', )):
-        super(ActorA, self).__init__()
+    def __init__(self, name=None):
+        super(Actor, self).__init__()
         self.__class__.count += 1
         self.id = "%s/%i" % (self.__class__.__name__, self.__class__.count)
-        self.logger = logging.getLogger(name)
-        self.name = name
-        self.config = config
-        self.in_ports = list(in_ports)
-        # list of connected ports
-        # results_id -> [target1, target2, ...]
+        self.name = name if name is not None else self.id
+        self.logger = logging.getLogger(self.name)
         self.connections = {}
         self.inputs = {}
-        self.clear_inputs()
 
     def put_input(self, port, value):
         self.logger.debug('put_input')
         self.inputs[port].append(value)
-        self.eval_inputs()
+        fire = self.eval_inputs()
+        if fire:
+            # notice supervisor here
+            self.fire()
+
+    def setup_input_ports(self, ports):
+        # setup dynamic input ports
+        self.inputs = {port: [] for port in ports}
+
+    def clear_inputs(self):
+        self.logger.debug('clear inputs')
+        for port in self.inputs:
+            self.inputs[port] = []
+
+
+class ActorA(Actor):
+    """docstring for ActorA"""
+
+    # fixed input ports as class attributes
+
+    def __init__(self, name, config, in_ports=('input_1', )):
+        super(ActorA, self).__init__(name=name)
+        self.config = config
+        self.setup_input_ports(in_ports)
 
     def eval_inputs(self):
         # input complete logic
         self.logger.debug('eval_inputs')
-        if self.inputs.get('input_1'):
-            self.fire()
-
-    def clear_inputs(self):
-        self.logger.debug('clear inputs')
-        for port in self.in_ports:
-            self.inputs[port] = []
+        return bool(self.inputs.get('input_1'))
 
     def fire(self, input_1=None):
-        self.logger.debug('inside fire')
         # keyword arguments enable direct calls
+        self.logger.debug('inside fire')
         if input_1 is None:
             input_1 = self.inputs['input_1']
         # for distributed runs a flag should be used for

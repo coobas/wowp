598e9b859c71a816670596ff0584b13e33bed412
Jan Pipek <jan.pipek@gmail.com>
2015-05-06 21:04:05 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='wowp',
+    version='0.0.20150506',
+    packages=find_packages(),
+    license='MIT',
+    description='Flow-based ...',
+    # long_description=open('README.md').read(),
+    author='Jakub Urban',
+    author_email='',
+    url='',
+    install_requires = [ ],
+    entry_points = {
+        # 'console_scripts' : [
+        #    'wowp = wowp:function_that_does_it_all'
+        # ]
+    }
+)

46fd2b458198d10cb2389576d30a6c6a586dbd09
Jan Pipek <jan.pipek@gmail.com>
2015-06-15 23:44:46 +0200

    IteratorActor

--- wowp/actors/experimental.py
+++ wowp/actors/experimental.py
@@ -20,9 +20,7 @@ class GeneratorActor(Actor):
 class LineReader(GeneratorActor):
     """Sequentially put all lines in a file."""
 
-    def __init__(self, name=None, inport_name="path", outport_name="line"):
-        if not name:
-            name = "LineReader"
+    def __init__(self, name="line_reader", inport_name="path", outport_name="line"):
         Actor.__init__(self, name=name)
         self.inports.append(inport_name)
         self.outports.append(outport_name)
@@ -35,3 +33,17 @@ class LineReader(GeneratorActor):
             for line in f:
                 yield self.outport_name, line.strip()
 
+
+class IteratorActor(GeneratorActor):
+
+    def __init__(self, name="iterator", inport_name="collection", outport_name="item"):
+        Actor.__init__(self, name)
+        self.inports.append(inport_name)
+        self.outports.append(outport_name)
+        self.inport_name = inport_name
+        self.outport_name = outport_name
+
+    def iterate(self):
+        collection = self.inports[self.inport_name].pop()
+        for item in collection:
+            yield self.outport_name, item

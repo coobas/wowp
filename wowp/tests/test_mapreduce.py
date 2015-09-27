2373e7ad70d31556056e828fdfdd915e7c4f3016
Jakub Urban <urban@ipp.cas.cz>
2015-09-27 20:36:36 +0200

    the first working Map actor: work in progress
    --HG--
    branch : unstable

---
+++ wowp/tests/test_mapreduce.py
@@ -0,0 +1,24 @@
+from wowp.actors import FuncActor
+from wowp.actors.mapreduce import Map  # , Reduce
+from wowp.schedulers import LinearizedScheduler
+from itertools import zip_longest
+from string import ascii_uppercase
+
+
+def test_map_run():
+    func = lambda x: x * 2
+    inp = range(5)
+    map_act = Map(FuncActor, args=(func, ), scheduler=LinearizedScheduler())
+    map_act.inports.inp.put(inp)
+    result = map_act.run()
+    assert all(a == b for a, b in zip_longest(result, map(func, inp)))
+
+
+# def test_map():
+#     map_actor = Map(FuncActor, args=(ascii_uppercase, ))
+#     test_str = 'abcdEFG'
+#     res = map_actor(test_str)
+#     assert res['out'] == test_str.upper()
+
+if __name__ == "__main__":
+    test_map_run()

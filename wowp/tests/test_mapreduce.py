6f86e8bd159348669a9e4537171af4102519b79d
Jakub Urban <urban@ipp.cas.cz>
2015-12-14 23:12:27 +0100

    Map + Concat are system actors: * seems to work with a simple test (not yet in tests)

--- wowp/tests/test_mapreduce.py
+++ wowp/tests/test_mapreduce.py
@@ -1,7 +1,9 @@
+import nose
 from wowp.actors import FuncActor
 from wowp.actors.mapreduce import Map  # , Reduce
 from wowp.schedulers import LinearizedScheduler
 from itertools import zip_longest
+import math
 from string import ascii_uppercase
 
 
@@ -11,7 +13,23 @@ def test_map_run():
     map_act = Map(FuncActor, args=(func,), scheduler=LinearizedScheduler())
     map_act.inports.inp.put(inp)
     result = map_act.run()
-    assert all(a == b for a, b in zip_longest(result, map(func, inp)))
+    assert all(a == b for a, b in zip_longest(result['out'], map(func, inp)))
+
+
+def test_map_linear():
+    ra = FuncActor(range)
+    func = math.sin
+    map_act = Map(FuncActor, args=(func,))
+    map_act.inports['inp'] += ra.outports['out']
+
+    wf = ra.get_workflow()
+    sch = LinearizedScheduler()
+
+    inp = 5
+    sch.run_workflow(wf, inp=inp)
+    result = wf.outports.out.pop_all()
+    assert len(result) == 1
+    assert all((a, b) for a, b in zip_longest(result[0], map(func, range(inp))))
 
 
 # def test_map():
@@ -20,5 +38,5 @@ def test_map_run():
 #     res = map_actor(test_str)
 #     assert res['out'] == test_str.upper()
 
-if __name__ == "__main__":
-    test_map_run()
+if __name__ == '__main__':
+    nose.run(argv=[__file__, '-vv'])

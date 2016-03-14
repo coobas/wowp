3df8d5a429d2159b6e6784f8e885611c41c0e80a
Jakub Urban <urban@ipp.cas.cz>
2016-03-15 00:47:11 +0100

    omfit actors new abstraction: iputs: name, command, input_files=(), output_files=(), workdir=None, shell='/bin/bash', print_output=True, timeout=None):
    --HG--
    branch : unstable

--- wowp/actors/omfit.py
+++ wowp/actors/omfit.py
@@ -4,12 +4,13 @@
 from wowp.components import Actor
 import six
 import os
+from wowp.logger import logger
 
 
 def _shell_run(command,
                workdir,
-               files_in='*',
-               files_out='*',
+               files_in=(),
+               files_out=(),
                timeout=None,
                shell='/bin/bash',
                print_output=True,
@@ -19,6 +20,10 @@ def _shell_run(command,
     import sys
     from tempfile import mkdtemp
     from glob import glob
+
+    if workdir is None:
+        # TODO handle creating new workdirs
+        raise NotImplementedError()
 
     tmpdir = mkdtemp(dir=workdir)
     print('tmpdir: {}'.format(tmpdir))
@@ -33,19 +38,31 @@ def _shell_run(command,
                 files_in = (files_in, )
             for glob_in in files_in:
                 print('glob_in: {}'.format(glob_in))
-                for in_file_name in glob(os.path.join(workdir, glob_in)):
-                    print('in_file_name: {}'.format(in_file_name))
-                    source = os.path.join(workdir, os.path.basename(in_file_name))
-                    target = os.path.join(tmpdir, os.path.basename(in_file_name))
-                    print('link({}, {}'.format(source, target))
+                if isinstance(glob_in, six.string_types):
+                    # use glob in this case, do not rename files
+                    # for in_file_name in glob(os.path.join(workdir, glob_in)):
+                    for in_file_name in glob(glob_in):
+                        print('in_file_name: {}'.format(in_file_name))
+                        # source = os.path.join(workdir, os.path.basename(in_file_name))
+                        source = os.path.abspath(in_file_name)
+                        target = os.path.join(tmpdir, os.path.basename(in_file_name))
+                        print('link({}, {})'.format(source, target))
+                        os.link(source, target)
+                else:
+                    # do not use glob if (source, target) filenames are provided
+                    # source = os.path.join(workdir, os.path.basename(glob_in[0]))
+                    source = os.path.abspath(glob_in[0])
+                    target = os.path.join(tmpdir, os.path.basename(glob_in[1]))
+                    print('link("{}", "{}")'.format(source, target))
                     os.link(source, target)
 
         except Exception:
-            print('Error linking input files')
+            logger.error('Error linking input files')
             raise
 
         # construct the full command, including timeout if provided
         if timeout is not None:
+            # TODO use Pythonic timeout rather than spawn
             full_command = ('''
     cd {}
     expect << EOF
@@ -90,18 +107,20 @@ def _shell_run(command,
         # link back output files
         if isinstance(files_out, six.string_types):
             files_out = (files_out, )
-        output_file_names = []
-        for out_glob in files_out:
-            for out_file_name in glob(os.path.join(tmpdir, out_glob)):
-                # TODO what about subdirectories?
-                source = os.path.join(tmpdir, os.path.basename(out_file_name))
-                target = os.path.join(workdir, os.path.basename(out_file_name))
-                if os.path.exists(target):
-                    # rename output file if it already exists
-                    # this means there was a CONFLICT
-                    target = target + '_' + os.path.basename(tmpdir)
-                os.link(source, target)
-                output_file_names.append(target)
+        # output_file_names = []
+        # for out_glob in files_out:
+        #     for out_file_name in glob(os.path.join(tmpdir, out_glob)):
+        #         # TODO what about subdirectories?
+        #         source = os.path.join(tmpdir, os.path.basename(out_file_name))
+        #         target = os.path.join(workdir, os.path.basename(out_file_name))
+        #         if os.path.exists(target):
+        #             # rename output file if it already exists
+        #             # this means there was a CONFLICT
+        #             target = target + '_' + os.path.basename(tmpdir)
+        #         os.link(source, target)
+        #         output_file_names.append(target)
+        output_file_names = [os.path.join(tmpdir, fout) for fout in files_out]
+
 
     finally:
         # TODO clean up tmpdir ???
@@ -118,29 +137,49 @@ def _shell_run(command,
 class TOQ(Actor):
     def __init__(self,
                  name='TOQ',
-                 binary='toq.x',
-                 toq_params=None,
+                 command='toq.x',
+                 input_files=(),
+                 output_files=(),
+                 workdir=None,
                  shell='/bin/bash',
                  print_output=True,
-                 timeout=120):
+                 timeout=None):
         super(TOQ, self).__init__(name=name)
-        self.inports.append('toq_in')
-        self.outports.append('toq_out')
 
-        self.binary = binary
-        self.toq_params = toq_params
+        # use input and output file names as ports
+        if isinstance(input_files, six.string_types):
+            input_files = (input_files, )
+        if isinstance(output_files, six.string_types):
+            output_files = (output_files, )
+        for in_file in input_files:
+            self.inports.append(in_file)
+        for out_file in output_files:
+            self.outports.append(out_file)
+
+        self.command = command
+        self.workdir = workdir
         self.shell = shell
         self.timeout = timeout
 
     def get_run_args(self):
         # get the input file name
-        file_in = self.inports['toq_in'].pop()
+        files_in = []
+        files_out = []
+        for inport in self.inports:
+            files_in.append((os.path.abspath(inport.pop()), inport.name))
+        for outport in self.outports:
+            files_out.append(outport.name)
         # assume working directory is the one that contains the input file
-        workdir = os.path.abspath(os.path.dirname(file_in))
+        # if files_in and os.path.isdir(os.path.dirname(files_in[0][0])):
+        #     workdir = os.path.abspath(os.path.dirname(files_in[0][0]))
+        # else:
+        #     # this will create a new workdir
+        #     workdir = None
         args = ()
-        kwargs = {'file_in': os.path.abspath(file_in),
-                  'workdir': workdir,
-                  'binary': self.binary,
+        kwargs = {'files_in': files_in,
+                  'files_out': files_out,
+                  'workdir': self.workdir,
+                  'command': self.command,
                   'shell': self.shell,
                   'timeout': self.timeout}
 
@@ -149,9 +188,9 @@ class TOQ(Actor):
     @staticmethod
     def run(*args, **kwargs):
 
-        shell_res = _shell_run(kwargs['binary'],
+        shell_res = _shell_run(kwargs['command'],
                                kwargs['workdir'],
-                               files_in=kwargs['file_in'],
+                               files_in=kwargs['files_in'],
                                files_out='toq_out',
                                timeout=kwargs['timeout'],
                                shell=kwargs['shell'],

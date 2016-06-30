cec41ebbdd02c9dfe908536ab4413791f48a3564
Jakub Urban <urban@ipp.cas.cz>
2016-06-30 15:01:38 +0200

    buf fix: (strange) Python 2.7 problem with type('Enum', ...)
    --HG--
    branch : unstable

--- wowp/util.py
+++ wowp/util.py
@@ -189,7 +189,7 @@ def enum(*sequential, **named):
     http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
     """
     enums = dict(zip(sequential, range(len(sequential))), **named)
-    return type('Enum', (), enums)
+    return type(str('Enum'), (), enums)
 
 
 MPI_TAGS = enum('READY', 'DONE', 'EXIT', 'START')
